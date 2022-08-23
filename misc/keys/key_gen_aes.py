90e025cf99c32dac714b9f1337e3d3db4aa6b8a1
Amn04 <abk8088@gmail.com>
2022-08-23 10:51:37 +0530

    parser added in keygen

--- misc/keys/key_gen_aes.py
+++ misc/keys/key_gen_aes.py
@@ -1,10 +1,15 @@
 from base64 import b64encode
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+import argparse
 
-key = get_random_bytes(16)
+parser = argparse.ArgumentParser()
+parser.add_argument('--length', type=int, required=True, help='Length of key')
+args = parser.parse_args()
 
+key = get_random_bytes(args.length)
 # Save the key to a file
 file_out = open("key_file.bin", "wb") # wb = write bytes
 file_out.write(key)
 file_out.close()
+print("Generated Key ",key)
