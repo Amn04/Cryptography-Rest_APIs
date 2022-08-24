f69782b6b79f6fb73a5c51894add68da23b19f12
Amn04 <abk8088@gmail.com>
2022-08-24 21:15:32 +0530

    documentation added and decryption working

--- flask_app/routes/symmetric/AES/AES_CCM/AES_CCM_decrypt.py
+++ flask_app/routes/symmetric/AES/AES_CCM/AES_CCM_decrypt.py
@@ -5,26 +5,55 @@ from base64 import b64decode
 from Crypto.Cipher import AES
 from main import app
 
-class Aes_decrypt_class(Resource):
+class Aes_decrypt_ccm(Resource):
     
     def get(self):
         key_state = False
-        app.logger.info("Decryption started")
+        app.logger.info("Inside decryption class")
         
         try:
             nonce = request.form.get('nonce')
             header = request.form.get('head')
             cipher_text = request.form.get('text')
             tag = request.form.get('tag')
-            key = request.form.get('key')
+            key = request.files.get('key')
 
-            cipher = AES.new(key, AES.MODE_CCM, nonce=b64decode(nonce))
+            if nonce == None or nonce=="" or nonce==" ":
+                return {'error':'nonce required in ccm'},201
+            else:
+                nonce=nonce
 
+            if header == None or header=="" or header==" ":
+                return {'error':'header required in ccm'},201
+            else:
+                header=header
+
+            if cipher_text == None or cipher_text=="" or cipher_text==" ":
+                return {'error':'cipher_text required in ccm'},201
+            else:
+                cipher_text=cipher_text
+
+            if tag == None or tag=="" or tag==" ":
+                return {'error':'tag required in ccm'},201
+            else:
+                tag=tag
+
+            if key == None or key=="" or key==" ":
+                return {'error':'Key required'},201
+            else:
+                key=key.read()
+
+            app.logger.info("Args check successfully")
+            
+            cipher = AES.new(key.read(), AES.MODE_CCM, nonce=b64decode(nonce))
             cipher.update(b64decode(header))
             plaintext = cipher.decrypt_and_verify(b64decode(cipher_text), b64decode(tag))
 
-            return {'message':'Decryption successfull',
-                    'plain_text': plaintext}, 200
+            app.logger.info("Decrption successful")
+
+            return {'message':'Decryption successful',
+                    'plain_text': str(plaintext)}, 200
         
         except Exception as e:
+            print(e)
             return {'error':'something went wrong'},500
--- flask_app/run.py
+++ flask_app/run.py
@@ -1,13 +1,15 @@
+from routes.symmetric.AES.AES_CCM.AES_CCM_decrypt import Aes_decrypt_ccm
 from main import app
 from flask_restful import Api
 
-from routes.symmetric.AES.AES_CCM.AES_CCM_encrypt import Aes_encrypt_class
+from routes.symmetric.AES.AES_CCM.AES_CCM_encrypt import Aes_encrypt_ccm
 from routes.home import Home
 
 api = Api(app)
 
 api.add_resource(Home,"/")
-api.add_resource(Aes_encrypt_class,"/symmetric/encrypt/aes")
+api.add_resource(Aes_encrypt_ccm,"/symmetric/encrypt/aes")
+api.add_resource(Aes_decrypt_ccm,"/symmetric/decrypt/aes")
 
 if __name__ == '__main__':
     app.logger.info("Flask App started successfully")
