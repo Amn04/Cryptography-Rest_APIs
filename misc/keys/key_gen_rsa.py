1a4493d53288635810620962c48d6eeb09fc62d6
Amn04 <abk8088@gmail.com>
2022-09-09 14:47:15 +0530

    now using cryptography instead pycypto

--- misc/keys/key_gen_rsa.py
+++ misc/keys/key_gen_rsa.py
@@ -1,15 +1,55 @@
-'''This is ann example key genrator file to generate new RSA keypair using pycrypto
-library of python. The key generated using this script will  be saved as mykey.pem
-in same directory. 
-This example also contains code to read the genarated key and create a object to use it.'''
+'''
+This iis an example of RSA keypair generation using cryptography.
+In this script private key is genarated with key size of 4096 and usiing that private
+key a public key is genarated for encryption.
+This example is taken from https://gist.github.com/gabrielfalcao/de82a468e62e73805c59af620904c124
+'''
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
-from Crypto.PublicKey import RSA
 
-key = RSA.generate(2048)
-f = open('mykey.pem','wb')
-f.write(key.export_key('PEM'))
-f.close()
+def utf8(s: bytes):
+    return str(s, 'utf-8')
 
-f = open('mykey.pem','r')
-key = RSA.import_key(f.read())
-print(key)
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=4096,
+    backend=default_backend()
+)
+public_key = private_key.public_key()
+
+
+private_pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+with open('private_key.pem', 'wb') as f:
+    f.write(private_pem)
+
+public_pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+with open('public_key.pem', 'wb') as f:
+    f.write(public_pem)
+
+
+with open("private_key.pem", "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
+        backend=default_backend()
+    )
+
+with open("public_key.pem", "rb") as key_file:
+    public_key = serialization.load_pem_public_key(
+        key_file.read(),
+        backend=default_backend()
+    )
