f8790783e24bec43ba6a7b9a377ba8e83c9ba537
Amn04 <abk8088@gmail.com>
2022-09-10 20:11:54 +0530

    RSA signing added not with perfection

---
+++ example/RSA/Sign/sign.py
@@ -0,0 +1,22 @@
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+
+with open("private_key.pem", "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
+        backend=default_backend()
+    )
+
+message = b"A message I want to sign"
+signature = private_key.sign(
+    message,
+    padding.PSS(
+        mgf=padding.MGF1(hashes.SHA256()),
+        salt_length=padding.PSS.MAX_LENGTH
+    ),
+    hashes.SHA256()
+)
+print("Signature: ",signature)
